35ef6956e7ae0e5ae3a0f3783d7052285d94c405
Rian Wouters <rian.wouters@asml.com>
2024-12-05 17:46:36 +0100

    small improvements

--- day04/Ceres Search p2.py
+++ day04/Ceres Search p2.py
@@ -1,16 +1,11 @@
 C = ["MAS", "SAM"]
 
 with open("input.txt") as f:
-    m = ["."+l[:-1] + "." for l in f.readlines()]
-    N = len(m)
-    m = [(N + 2)*"."] + m + [(N + 2)*"."]
-    r = 0
-    for i in range(1, N + 1):
-        for j in range(1,N + 1):
+    m = [l[:-1] for l in f.readlines()]
+    s,r = 0, range(1, len(m) - 1)
+    for i in r:
+        for j in r:
             d1, d2 = m[i-1][j-1] + m[i][j] + m[i+1][j+1], m[i-1][j+1] + m[i][j] + m[i+1][j-1] 
             if d1 in C and d2 in C:
-                r += 1
-    print(r)
-
-
-
+                s += 1
+    print(s)
