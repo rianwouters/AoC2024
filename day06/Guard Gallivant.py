7cd1d92a82eacd3cfb0621c65337990a5ab13e08
Rian Wouters <rian.wouters@asml.com>
2024-12-06 15:59:44 +0100

    get rid of len calls

--- day06/Guard Gallivant.py
+++ day06/Guard Gallivant.py
@@ -1,8 +1,8 @@
 deltas = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 def walk(m, y, x):
-    d, dy, dx, visited = 0, -1, 0, {(y, x, 0)}
-    while 0 <= (nx := x + dx) < len(m[0]) and 0 <= (ny := y + dy) < len(m):
+    N, M, d, dy, dx, visited = len(m[0]), len(m), 0, -1, 0, {(y, x, 0)}
+    while 0 <= (nx := x + dx) < N and 0 <= (ny := y + dy) < M:
         if m[ny][nx] == '.':
             y, x = ny, nx
         else: 
@@ -13,7 +13,7 @@ def walk(m, y, x):
         visited.add((y, x, d))            
     return visited
 
-with open("test.txt") as f:
+with open("input.txt") as f:
     r, m = 0, [l[:-1] for l in f.readlines()]
     sy, sx = next(((y, x) for y, l in enumerate(m) if (x := l.find('^')) >= 0))
     m[sy] = m[sy][:sx] + "." + m[sy][sx + 1:]
