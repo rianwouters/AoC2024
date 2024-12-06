23f0ebce6d46111f795756c76d6f2ff8a5d41202
Rian Wouters <rian.wouters@asml.com>
2024-12-06 15:49:19 +0100

    performance improvement

--- day06/Guard Gallivant.py
+++ day06/Guard Gallivant.py
@@ -1,7 +1,7 @@
 deltas = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 def walk(m, sy, sx):
-    y, x, d, dy, dx, visited = sy, sx, 0, -1, 0, [(sy, sx, 0)]
+    y, x, d, dy, dx, visited = sy, sx, 0, -1, 0, {(sy, sx, 0)}
     while 0 <= (nx := x + dx) < len(m[0]) and 0 <= (ny := y + dy) < len(m):
         if m[ny][nx] == '.':
             y, x = ny, nx
@@ -10,10 +10,10 @@ def walk(m, sy, sx):
             dy, dx = deltas[d]
         if (y, x, d) in visited:
             return
-        visited.append((y, x, d))            
+        visited.add((y, x, d))            
     return visited
 
-with open("test.txt") as f:
+with open("input.txt") as f:
     r, m = 0, [l[:-1] for l in f.readlines()]
     sy, sx = next(((y, x) for y, l in enumerate(m) if (x := l.find('^')) >= 0))
     m[sy] = m[sy][:sx] + "." + m[sy][sx + 1:]
