f2bc7578b0567dae261d145d87140493a2828a6e
Rian Wouters <rian.wouters@asml.com>
2024-12-06 15:05:26 +0100

    improvements

--- day06/Guard Gallivant p2.py
+++ day06/Guard Gallivant p2.py
@@ -1,20 +1,17 @@
 deltas = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 def walk(m, sy, sx):
-    y, x, d, visited = sy, sx, 0, [(sy, sx, 0)]
-    while True:
-        delta = deltas[d]
-        ny, nx = y + delta[0], x + delta[1]
-        if not(0 <= nx < len(m[0]) and 0 <= ny < len(m)):
-            return visited
+    y, x, d, dy, dx, visited = sy, sx, 0, -1, 0, [(sy, sx, 0)]
+    while 0 <= (nx := x + dx) < len(m[0]) and 0 <= (ny := y + dy) < len(m):
         if m[ny][nx] == '.':
             y, x = ny, nx
         else: 
             d = (d + 1) % 4
+            dy, dx = deltas[d]
         if (y, x, d) in visited:
-            return True
-        else:
-            visited.append((y, x, d))            
+            return "LOOPING"
+        visited.append((y, x, d))            
+    return visited
 
 with open("test.txt") as f:
     m = [l[:-1] for l in f.readlines()]
@@ -26,7 +23,7 @@ with open("test.txt") as f:
     r = 0
     for n, (oy, ox) in enumerate(route[1:]):
         t, m[oy] = m[oy], m[oy][:ox] + "#" + m[oy][ox + 1:]
-        if walk(m, sy, sx) == True:
+        if walk(m, sy, sx) == "LOOPING":
             r += 1
         m[oy] = t
     print("part 2: ", r)
