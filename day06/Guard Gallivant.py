d3a8827af107b516f077583fafdae8356bbad71f
Rian Wouters <rian.wouters@asml.com>
2024-12-06 15:11:46 +0100

    remove specific p1 solution

--- day06/Guard Gallivant.py
+++ day06/Guard Gallivant.py
@@ -1,21 +1,29 @@
-with open("input.txt") as f:
-    m = [l[:-1] for l in f.readlines()]
-    y, x = next((n, l.find('^')) for n, l in enumerate(m) if l.find('^') >= 0)
-    m[y] = m[y].replace("^", ".")
-    deltas = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    d, r, visited = 0, 0, [(y, x)]
-    while True:
-        delta = deltas[d]
-        ny, nx = y + delta[0], x + delta[1]
-        if not(0 <= nx < len(m[0]) and 0 <= ny < len(m)):
-            break
+deltas = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+def walk(m, sy, sx):
+    y, x, d, dy, dx, visited = sy, sx, 0, -1, 0, [(sy, sx, 0)]
+    while 0 <= (nx := x + dx) < len(m[0]) and 0 <= (ny := y + dy) < len(m):
         if m[ny][nx] == '.':
             y, x = ny, nx
-            if (y, x) not in visited:
-                visited.append((y, x))
         else: 
             d = (d + 1) % 4
-            
+            dy, dx = deltas[d]
+        if (y, x, d) in visited:
+            return "LOOPING"
+        visited.append((y, x, d))            
+    return visited
 
-    print(visited)
-    print(len(visited))
+with open("test.txt") as f:
+    m = [l[:-1] for l in f.readlines()]
+    sy, sx = next(((y, x) for y, l in enumerate(m) if (x := l.find('^')) >= 0))
+    m[sy] = m[sy][:sx] + "." + m[sy][sx + 1:]
+    route = list(set((x,y) for x, y, d in walk(m, sy, sx)))
+    print("part 1:", len(route))
+
+    r = 0
+    for n, (oy, ox) in enumerate(route[1:]):
+        t, m[oy] = m[oy], m[oy][:ox] + "#" + m[oy][ox + 1:]
+        if walk(m, sy, sx) == "LOOPING":
+            r += 1
+        m[oy] = t
+    print("part 2: ", r)
