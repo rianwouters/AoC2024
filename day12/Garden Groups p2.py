b7ae0e22ee923f3f664dcfa660fba4676f6f05d2
Rian Wouters <rian.wouters@asml.com>
2024-12-13 11:54:02 +0100

    refactor a bit

--- day12/Garden Groups p2.py
+++ day12/Garden Groups p2.py
@@ -1,5 +1,4 @@
 from itertools import groupby
-from operator import itemgetter
 from os.path import join, dirname
 
 from utils.aoc import add_borders, get_matrix 
@@ -9,25 +8,23 @@ with open(join(dirname(__file__), "input.txt")) as f:
     m = get_matrix(f)
     m = add_borders(m)
     N, M = len(m), len(m[0])
-    b, r, r2 = [M*[True] for n in range(N)], 0, 0
-    for y in range(1,N-1):
-        for x in range(1, M-1):
+    b = [M*[True] for n in range(N)]
+    r = r2 = 0
+    for y in range(1, N - 1):
+        for x in range(1, M - 1):
             if b[y][x]:
-                b[y][x], visited, k, p, perimeter = False, [(y,x)], 0, 0, []
-                while k < len(visited):
-                    u, v = visited[k]
-                    for d, s, t, py, px in [(0, u, v + 1, u, v),(2, u, v - 1, u, v-1), (1, u - 1, v, u - 1, v), (3, u + 1, v, u, v)]:
+                b[y][x], visited, a, perimeter = False, [(y,x)], 0, []
+                while a < len(visited):
+                    u, v = visited[a]
+                    for d, (s, t, py, px) in enumerate([(u, v + 1, u, v),(u - 1, v, u - 1, v), (u, v - 1, u, v - 1), (u + 1, v, u, v)]):
                         if m[s][t] == m[y][x]:
                             if b[s][t]:
                                 visited.append((s,t))
                                 b[s][t] = False
                         else:
                             perimeter.append((d ,py if d % 2 else px, px if d % 2 else py))
-                            p += 1
-                    k += 1
-                a = len(visited)
-                p2 = len(list(groupby(enumerate(sorted(perimeter)), lambda p: (p[1][0], p[1][1], p[0]-p[1][2]))))                                               
-                r += a * p
-                r2 += a * p2
+                    a += 1
+                r += a * len(perimeter)
+                r2 += a * sum(1 for _ in groupby(enumerate(sorted(perimeter)), lambda p: (p[1][0], p[1][1], p[0]-p[1][2])))
     print("part 1: ", r)
     print("part 2: ", r2)
