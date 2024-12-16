84ab7617345d0a5ec3e01c23d44cd814ea7c0573
Rian Wouters <rian.wouters@asml.com>
2024-12-16 12:01:57 +0100

    remove comment

--- day13/Claw Contraption .py
+++ day13/Claw Contraption .py
@@ -20,30 +20,3 @@ with open(join(dirname(__file__), "input.txt")) as f:
     except:
         pass
     print(r)
-# px = ax * a + bx * b
-# py = ay* a + by * b
-
-# px = ax * a + bx * b
-# (ax/ay) * py * (ax/ay) * ay* a + (ax/ay) * by * b
-
-# px - (ax/ay) * py = bx * b - (ax/ay) * by * b
-# ay*px - ax * py = (ay*bx - ax * by) * b
-# b = (ay*px - ax * py) / (ay*bx - ax * by) 
-
-# a = (py - by * b) / ay = py/ay - by/ay * (ay*px - ax * py) / (ay*bx - ax * by)* 
-
-
-
-# 8400 = 94a + 22b
-# 5400 = 34a + 67b) *(94/34) = 5400 * (94/34) = 94a + 67*(94/34)b
-
-# 8400 - 5400 * (94/34) = 22b - 67*(94/34)b
-
-# 8400 - 5400 * (94/34) = (22 - 67*(94/34)) * b
-# b
-# = 8400 - 5400 * (94/34) / (22 - 67*(94/34))
-# = 34*8400 - 5400 * 94 / 22*34 - 67*94)
-# 
-# a = (8400 - 22b) / 94
-
-
